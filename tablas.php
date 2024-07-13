4efb1667cfb24ac667959f3222be0291188d36e2
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-07-13 16:02:26 -0500

    Added coutry

--- tablas.php
+++ tablas.php
@@ -156,7 +156,10 @@ ORDER BY p.id;
         '{{Bandera|Bélgica}}{{Bandera|Luxemburgo}} [[Unión Económica Belgo-Luxemburguesa|' . htmlspecialchars(trim($row['Pais'])) . ']]' :
         (trim($row['Pais']) === 'República Democrática Popular de Etiopía' ?
             '{{Bandera|Etiopía|1975-estado}} [[República Democrática Popular de Etiopía|' . htmlspecialchars(trim($row['Pais'])) . ']]' :
-            '{{Bandera2|' . htmlspecialchars(trim($row['Pais'])) . '}}'
+            (trim($row['Pais']) === 'República Democrática del Sudán' ?
+                '{{Bandera|Sudán}} [[República Democrática del Sudán|' . htmlspecialchars(trim($row['Pais'])) . ']]' :
+                '{{Bandera2|' . htmlspecialchars(trim($row['Pais'])) . '}}'
+            )
         )
     ) :
     htmlspecialchars(trim($row['Pais']))
