193110a5eddebac8ff5249ff89c85e41139f6ed3
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-08-24 16:20:54 -0500

    Removing

--- summary.php
+++
@@ -1,2 +0,0 @@
-<?php
-$texto_anexo = "Esta es una lista histórica de países por producción de \$producto, basada en los datos de la Organización de las Naciones Unidas para la Alimentación y la Agricultura. La producción mundial total de \$producto en \$año era de \$toneladas toneladas. \$país es el mayor productor, representando el \$porcentaje% de la producción mundial. Los territorios dependientes son mostrados en cursiva.";
