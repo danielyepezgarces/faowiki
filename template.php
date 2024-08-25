8e04c89233625d7de901b0e4e90b6f3caa81c51c
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-08-25 00:21:58 -0500

    Add css

--- template.php
+++ template.php
@@ -6,9 +6,7 @@
     <title><?php echo $page_title; ?></title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        /* Custom styles here */
-    </style>
+    <link href="/css/tables.css" rel="stylesheet">
 </head>
 <body>
     <div class="content">
