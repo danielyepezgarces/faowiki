c3a805cff3cb4380e1a212c9eca9e2573149ba9f
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-08-26 17:49:03 -0500

    Update tablas.php

--- raw/tablas.php
+++ raw/tablas.php
@@ -129,7 +129,7 @@ if ($itemCode) {
         $mediaWikiTable = htmlTableToMediaWiki($htmlTable);
         
         // Add the entradilla content before the table
-        echo $entradilla . "\n\n== Tabla ==" . $mediaWikiTable;
+        echo $entradilla . "\n\n== Tabla ==\n" . $mediaWikiTable;
     } else {
         echo "Failed to retrieve HTML content from the URL.";
     }
