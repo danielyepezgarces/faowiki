50d44b1e7d499eb9e52bf39a02d500d72108443c
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-08-26 19:28:02 -0500

    Update template.php

--- template.php
+++ template.php
@@ -23,7 +23,7 @@ $percentage_highest_producer = get_highest_producer_percentage($conn, $item_code
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/tables.css" rel="stylesheet">
 
 <body>
