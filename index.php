d2280080afbd8ee6fcb3ec5023451b753e0d9335
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-08-26 19:28:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FAOWIKI - Empowering Knowledge with FAO’s Global Data</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .header {
             text-align: center;
